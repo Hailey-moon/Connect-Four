033730f935e6a63d16b600e58e808fc055f080a4
Hailey Moon <82325205+Hailey-moon@users.noreply.github.com>
2022-05-17 11:12:07 -0400

    edited main.py

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from AIPlayer import *
 import random
 
 
+# mode 1: Two Player
 # uncomment to play as two-player
 # connect_four(Player('X'), Player('O'))
 
 
+# mode 2: Single Player vs Random Player
 # uncomment to play with random player
 # connect_four(Player('X'), RandomPlayer('O'))
 
+# mode 3: Single Player vs AI Player
 # uncomment to play with AI player
 # connect_four(Player('X'), AIPlayer('O', 'RANDOM', 5))
